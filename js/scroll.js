597d41cf5738954e59fed0c88cd481fe2c9b3f89
elo-world <175364541+elo-world@users.noreply.github.com>
2026-03-06 08:49:16 +0100

    Responsive

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,24 @@
-window.addEventListener("scroll", (e) => {
-    if (2 * window.scrollY < window.innerHeight) {
-        navbar.style.opacity = `${1 - (2 * window.scrollY) / window.innerHeight}`;
-        navbar.style.display = "flex";
-        burger_menu.style.display = "none";
+const navbar_scroll = () => {
+    console.log(window.innerWidth);
+
+    if (window.innerWidth > 970) {
+        if (2 * window.scrollY < window.innerHeight) {
+            navbar.style.opacity = `${1 - (2 * window.scrollY) / window.innerHeight}`;
+            navbar.style.display = "flex";
+            burger_menu.style.display = "none";
+        } else {
+            burger_menu.style.opacity = `${(2 * (window.scrollY - window.innerHeight / 2)) / window.innerHeight}`;
+            navbar.style.display = "none";
+            burger_menu.style.display = "flex";
+        }
     } else {
-        burger_menu.style.opacity = `${(2 * (window.scrollY - window.innerHeight / 2)) / window.innerHeight}`;
         navbar.style.display = "none";
         burger_menu.style.display = "flex";
     }
+};
+
+window.addEventListener("scroll", () => {
+    navbar_scroll();
 });
+
+navbar_scroll();
